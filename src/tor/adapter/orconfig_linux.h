2841319d565ee1a6abe8463b21dd4ff00402a7e0
BreakoutCoin <BreakoutCoin@users.noreply.github.com>
2022-02-03 01:50:59 -0700

    Adding some coverage for PC_FROM_UCONTEXT

--- src/tor/adapter/orconfig_linux.h
+++ src/tor/adapter/orconfig_linux.h
@@ -1,6 +1,9 @@
 /* orconfig.h.  Generated from orconfig.h.in by configure.  */
 /* orconfig.h.in.  Generated from configure.ac by autoheader.  */
 
+#define __USE_GNU
+#include <ucontext.h>
+
 #include <openssl/opensslv.h>
 
 /* Define if building universal (internal helper macro) */
